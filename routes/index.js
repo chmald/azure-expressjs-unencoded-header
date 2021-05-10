6230311c2c6e1614be601d77df0d96f8144512bd
Christopher Maldonado <chmald@microsoft.com>
2021-05-10 11:22:38 -0500

    testing

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 // Middleware for Azure Header
 router.use(function(req, res, next) {
-  console.log("req original path", req.url);
+  console.log("req original path", req.path);
   console.log("req unencoded path", req.headers['x-waws-unencoded-url']);
 
   if(req.headers['x-waws-unencoded-url'] != undefined) {
     req.originalUrl = req.headers['x-waws-unencoded-url'];
-    req.url = req.headers['x-waws-unencoded-url'];
+    req.path = req.headers['x-waws-unencoded-url'];
     console.log("req path updated");
     console.log("req new path", req.url);
   }
@@ -19,6 +19,14 @@ router.use(function(req, res, next) {
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/path/:info*', function(req, res, next) {
+  console.log("path", req.params['info']);
+  res.json({
+    id: req.params['info'],
+    path: req.params[0]
+  });
 });
 
 module.exports = router;
