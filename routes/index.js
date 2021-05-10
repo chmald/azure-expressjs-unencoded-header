4cc9406bb964f327a2df589357fa51f8cbcb4fcd
Christopher Maldonado <chmald@microsoft.com>
2021-05-10 11:04:49 -0500

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 // Middleware for Azure Header
 router.use(function(req, res, next) {
-  console.log("request", req)
+  console.log("req original path", req.url);
+  console.log("req unencoded path", req.headers['x-waws-unencoded-url']);
+
+  if(req.headers['x-waws-unencoded-url'] != undefined) {
+    req.url = req.headers['x-waws-unencoded-url'];
+    console.log("req path updated");
+  }
+
   next();
 });
 
